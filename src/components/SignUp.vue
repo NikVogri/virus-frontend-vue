85a3fa5551ca96cda599441bc2d39cb8e074c82f
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-11-07 23:43:45 +0100

    feat: shows correct message

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -4,19 +4,21 @@
     <div class="card-body p-4 p-md-5">
           <h4>Sign up and receive daily updates</h4>
           <p class="mb-5">Receive updates for your selected countries every single day at 2 PM CET</p>
-              <form @submit.prevent="confirm">
+              <form @submit.prevent="confirm" class="m-0">
                 <input type="email" placeholder="Your email adress..." class="form-control" v-model="emailInput">
                 <div class="d-flex mt-2">
-                  <CountryAutoComplete @inputChanged="changeInput" class="mt-3"/>
+                  <CountryAutoComplete @inputChanged="changeInput" class="mt-3" />
                 <button class="btn btn-info mt-3" type="button" @click="addCountryToList">Add To List</button>
                 </div>
                 <ul class="country-list">
                   <li v-for="country in countryList" :key="country" @click="removeCountryFromList(country)">{{country}}</li>
                 </ul>
-                <button v-if="countryList.length && emailInput" type="submit" class="btn btn-outline-danger active mt-4">
+                <button v-if="showAddButton && emailInput && countryList.length" type="submit" class="btn btn-outline-danger active mt-4">
                   Confirm
                 </button>
               </form>
+
+              <p v-if="notification.msg" :class="{'text-danger': notification.type == 'warning', 'text-success': notification.type == 'success'}">{{ notification.msg }}</p>
         </div>
   </div>
 </template>
@@ -30,33 +32,53 @@ export default {
     return {
       input: '',
       emailInput: '',
-      countryList: []
+      countryList: [],
+      notification: {type: '', msg: ''},
+      showAddButton: true
     }
   },
   components: {
     CountryAutoComplete
   },
+  watch: {
+    emailInput: function() {
+      if (!this.showAddButton) {
+        this.showAddButton = true;
+      }
+    }
+  },
   methods: {
     changeInput(input) {
+      console.log('hgere');
       this.input = input;
     },
     addCountryToList() {
-      this.countryList.push(this.input);
-      this.input = '';
+
+      if (this.countryList.indexOf(this.input) == -1 && this.input) {
+        this.countryList.push(this.input);
+        this.input = '';
+      }
     },
     removeCountryFromList(country) {
       this.countryList = this.countryList.filter(listCountry => listCountry !== country);
     },
-    confirm() {
+    async confirm () {
       const data = {
         countries: this.countryList,
         email: this.emailInput
       };
 
-      console.log(data);
+      try{
+        const res = await axios.post('https://virus-email-sender.herokuapp.com/add-email', data)
+        this.notification = {type: 'success', msg: res.data.msg};
+        this.showAddButton = false;
+        setTimeout(this.closeCard, 5000);
+      } catch(err) {
+        this.showAddButton = false;
+        this.notification = {type: 'warning', msg: err.response.data.msg};
+      }
 
-      axios.post('https://virus-email-sender.herokuapp.com/add-email', data).then(res => this.message = res.message).catch(err => this.message = err.message);
-    }, 
+    },
     closeCard() {
       localStorage.setItem('showEmailCard', false);
       this.$emit('hideEmailNotification');
@@ -89,10 +111,12 @@ export default {
     background: #03a9f4;
     display: inline-block;
     cursor: pointer;
+    margin-bottom: 5px;
 
     &:not(:last-of-type){
     margin-right: 5px;
     }
   }
 }
+
 </style>
